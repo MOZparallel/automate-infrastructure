1b4ffd37adcfc58ce7ad28fcb565a47d39987b41
Harshada Ramesh Haryan <118703416+MOZparallel@users.noreply.github.com>
2025-10-24 11:07:35 +0530

    Update outputs.tf

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -9,9 +9,3 @@ output "rds_id" {
   description = "The ID of the primary RDS instance"
   value       = aws_db_instance.main.id
 }
-
-#output "replica_endpoint" {
- # description = "The endpoint of the read replica"
- # value       = aws_db_instance.replica.endpoint
-#}
-
